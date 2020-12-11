61d7ec2a546b3befcd2ec0256f03f7beae066786
an0ri0n <64656269+an0ri0n@users.noreply.github.com>
2020-12-12 02:36:24 +0600

    info component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { AdminHomeComponent } from './components/admin-home/admin-home.component';
 import { AdminGuardGuard } from './admin-guard.guard';
+import {InfoComponent} from './components/info/info.component';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'admin', component: AdminHomeComponent, canActivate: [AdminGuardGuard]},
+  {path: 'info', component: InfoComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { FooterComponent } from './components/footer/footer.component';
 import { LoginComponent } from './components/login/login.component';
 import { AdminHomeComponent } from './components/admin-home/admin-home.component';
 import { HttpClientModule} from '@angular/common/http';
+import { InfoComponent } from './components/info/info.component';
 
 @NgModule({
   declarations: [
@@ -17,6 +18,7 @@ import { HttpClientModule} from '@angular/common/http';
     FooterComponent,
     LoginComponent,
     AdminHomeComponent,
+    InfoComponent,
   ],
   imports: [
     BrowserModule,
